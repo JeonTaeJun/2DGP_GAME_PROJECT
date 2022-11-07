d6c0565084b493cbfa35b887627d92d150f0198d
JeonTaeJun <wjs1xo2wns3@naver.com>
2022-11-08 04:05:24 +0900

    change random circle add

--- enemies.py
+++ enemies.py
@@ -2,21 +2,31 @@ from pico2d import *
 import random
 import os
 import math
-
+import collide
 import game_world
-import main
+import play_state
 import Map
-class skeleton :
+
+
+deg=None
+def random_deg():
+    global deg
+    deg = random.randint(0,360)
+    return deg
+class skeleton_many :
     image = None
     def __init__(self):
         self.frame = 0
         self.direction =0
-        self.x, self.y = random.randint(100, 1240) , random.randint(1024, 1124)
-        self.dx =0
-        self.dy =0
+        self.x, self.y = random.randint(play_state.player.player_x-300,play_state.player.player_x+300) ,\
+                         random.randint(-200,0)
+        self.dx = 0
+        self.dy = 0
+        self.speed = 15
         self.HP = 20
-        if skeleton.image == None:
-            skeleton.image = load_image('skelton.png')
+        self.power = 5
+        if skeleton_many.image == None:
+            skeleton_many.image = load_image('skelton.png')
     def get_bb(self):
         return self.x-15, self.y-15, self.x+15, self.y+15
 
@@ -24,31 +34,68 @@ class skeleton :
         self.image.clip_draw(self.frame*30,self.direction*56,30,56,self.x,self.y,50,60)
         draw_rectangle(*self.get_bb())
 
-    def update(self):#, player_x, player_y):
-        self.dx, self.dy = (( main.player.player_x - self.x) / math.sqrt((main.player.player_x - self.x) ** 2 + (main.player.player_y - self.y) ** 2),
-                            (main.player.player_y - self.y) / math.sqrt((main.player.player_x - self.x) ** 2 + (main.player.player_y - self.y) ** 2))
-
-        self.x += self.dx * 2
-        self.y += self.dy * 2
-        self.x -= main.player.dx
-        self.y -= main.player.dy
+    def update(self):
+        self.y += self.speed
+        self.x -= play_state.player.dx
+        self.y -= play_state.player.dy
         if 640 > self.x:
             self.direction = 0
         elif 640 < self.x:
             self.direction = 1
         self.frame = (self.frame + 1) % 6
-        if self.HP < 0:
+
+        if self.HP<0:
             game_world.remove_object(self)
 
+class skeleton :
+    image = None
+    def __init__(self):
+        self.deg = random_deg()
+        self.frame = 0
+        self.direction =0
+        self.x, self.y = play_state.player.player_x+(math.cos(math.radians(self.deg))*800),\
+                         play_state.player.player_y+(math.sin(math.radians(self.deg))*800)
+        self.dx = 0
+        self.dy = 0
+        self.HP = 20
+        self.power = 5
+        if skeleton.image == None:
+            skeleton.image = load_image('skelton.png')
+    def get_bb(self):
+        return self.x-15, self.y-15, self.x+15, self.y+15
+
+    def draw(self):
+        self.image.clip_draw(self.frame*30,self.direction*56, 30, 56, self.x, self.y, 50, 60)
+        draw_rectangle(*self.get_bb())
+
+    def update(self):#, player_x, player_y):
+        self.dx, self.dy = (( play_state.player.player_x - self.x) / math.sqrt((play_state.player.player_x - self.x) ** 2 + (play_state.player.player_y - self.y) ** 2),
+                            (play_state.player.player_y - self.y) / math.sqrt((play_state.player.player_x - self.x) ** 2 + (play_state.player.player_y - self.y) ** 2))
+
+        self.x += self.dx * 2
+        self.y += self.dy * 2
+        self.x -= play_state.player.dx
+        self.y -= play_state.player.dy
+        if 640 > self.x:
+            self.direction = 0
+        elif 640 < self.x:
+            self.direction = 1
+        self.frame = (self.frame + 1) % 6
+
+        if self.HP<0:
+            game_world.remove_object(self)
 class zombie:
     image = None
     def __init__(self):
+        self.deg = random_deg()
         self.frame = 0
-        self.direction =0
-        self.x, self.y = random.randint(1240, 1340) , random.randint(0, 1024)
-        self.dx =0
+        self.direction = 0
+        self.x, self.y = play_state.player.player_x+(math.cos(math.radians(self.deg))*800),\
+                         play_state.player.player_y+(math.sin(math.radians(self.deg))*800)
+        self.dx = 0
         self.dy =0
-        self.HP=30
+        self.HP= 30
+        self.power = 10
         if zombie.image == None:
             zombie.image = load_image('zombie1.png')
     def draw(self):
@@ -58,17 +105,18 @@ class zombie:
     def get_bb(self):
         return (self.x-15,self.y-20,self.x+15,self.y+20)
     def update(self):#, player_x, player_y):
-        self.dx, self.dy = ((main.player.player_x-self.x)/math.sqrt((main.player.player_x-self.x)** 2+(main.player.player_y-self.y) ** 2),
-                          (main.player.player_y-self.y)/math.sqrt((main.player.player_x-self.x)** 2+(main.player.player_y-self.y) ** 2))
+        self.dx, self.dy = ((play_state.player.player_x-self.x)/math.sqrt((play_state.player.player_x-self.x)** 2+(play_state.player.player_y-self.y) ** 2),
+                          (play_state.player.player_y-self.y)/math.sqrt((play_state.player.player_x-self.x)** 2+(play_state.player.player_y-self.y) ** 2))
 
-        self.x+=self.dx*2
-        self.y+=self.dy*2
-        self.x -= main.player.dx
-        self.y -= main.player.dy
+        self.x += self.dx*2
+        self.y += self.dy*2
+        self.x -= play_state.player.dx
+        self.y -= play_state.player.dy
         if 640 > self.x:
             self.direction = 0
         elif 640 < self.x:
             self.direction = 1
         self.frame = (self.frame + 1) % 6
-        if self.HP <0:
+
+        if self.HP<0:
             game_world.remove_object(self)
